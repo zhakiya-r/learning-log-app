2b541030e72e512271e56bb79936ca9d01805897
zhakiya-r <z.rakhymzhan@yahoo.com>
2026-05-30 17:05:02 +0500

    feat(topics): create all topics page

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,6 @@ app_name = "learning_logs"
 urlpatterns = [
     # Home page
     path("", views.index, name="index"),
+    # Page with a list of all topics
+    path("topics/", views.topics, name="topics"),
 ]
