4458443c85eb0b9898345d995466d911faf2300e
zhakiya-r <z.rakhymzhan@yahoo.com>
2026-05-30 16:27:46 +0500

    feat(topic): define Topic model and activate app

---
+++ learning_logs/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Topic(models.Model):
+    """Topic the user is studying"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Returns a string representation of the model"""
+        return self.text
