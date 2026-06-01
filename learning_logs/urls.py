484a7afc0a424141dbd6af29ad6266be6d48117d
zhakiya-r <z.rakhymzhan@yahoo.com>
2026-06-01 12:13:35 +0500

    feat(topics): add topic creation form (for users)

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path("topics/", views.topics, name="topics"),
     # Page with detailed information for a specific topic
     path("topics/<int:topic_id>/", views.topic, name="topic"),
+    # Page for adding a new topic
+    path("new_topic/", views.new_topic, name="new_topic"),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
+from .forms import TopicForm
 
 def index(request):
     """Home page for the "Learning Logs" application"""
@@ -18,3 +19,18 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by("-date_added")
     context = {"topic":topic, "entries":entries}
     return render(request, "learning_logs/topic.html", context)
+
+def new_topic(request):
+    """Adds a new topic"""
+    if request.method != "POST":
+        # No data was submitted; an empty form is created
+        form = TopicForm()
+    else:
+        # POST data submitted; process the data
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("learning_logs:topics")
+    # Display an empty or invalid form
+    context = {"form":form}
+    return render(request, "learning_logs/new_topic.html", context)
