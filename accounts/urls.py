c29cf0f8f0899eb69677c1b213c5c4d3e88a0a2f
zhakiya-r <z.rakhymzhan@yahoo.com>
2026-06-04 20:58:29 +0500

    feat(auth): implement user registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,12 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = "accounts"
 urlpatterns = [
     # Add the default authentication URLs
     path("", include("django.contrib.auth.urls")),
+    # Registration page
+    path("register/", views.register, name="register"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def register(request):
+    """Registers a new user"""
+    if request.method != "POST":
+        # Display an empty registration form
+        form = UserCreationForm()
+    else:
+        # Processing the submitted form
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Log the user in and redirect to the home page
+            login(request, new_user)
+            return redirect("learning_logs:index")
+        
+    # Display an empty or invalid form
+    context = {"form": form}
+    return render(request, "registration/register.html", context)
