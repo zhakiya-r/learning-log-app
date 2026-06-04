50c7d0f0a8b1371ebc44904f189b1fb1448059c8
zhakiya-r <z.rakhymzhan@yahoo.com>
2026-06-04 21:08:36 +0500

    feat(auth): restrict access to authenticated users

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -7,12 +8,14 @@ def index(request):
     """Home page for the "Learning Logs" application"""
     return render(request, "learning_logs/index.html")
 
+@login_required
 def topics(request):
     """Displays a list of topics"""
     topics = Topic.objects.order_by("date_added")
     context = {"topics":topics}
     return render(request, "learning_logs/topics.html", context)
 
+@login_required
 def topic(request, topic_id):
     """Displays a topic and all its entries"""
     topic = Topic.objects.get(id=topic_id)
@@ -20,6 +23,7 @@ def topic(request, topic_id):
     context = {"topic":topic, "entries":entries}
     return render(request, "learning_logs/topic.html", context)
 
+@login_required
 def new_topic(request):
     """Adds a new topic"""
     if request.method != "POST":
@@ -35,6 +39,7 @@ def new_topic(request):
     context = {"form":form}
     return render(request, "learning_logs/new_topic.html", context)
 
+@login_required
 def new_entry(request, topic_id):
     """Adds a new entry for a specific topic"""
     topic = Topic.objects.get(id=topic_id)
@@ -54,6 +59,7 @@ def new_entry(request, topic_id):
     context = {"topic":topic, "form":form}
     return render(request, "learning_logs/new_entry.html", context)
 
+@login_required
 def edit_entry(request, entry_id):
     """Edits an existing entry"""
     entry = Entry.objects.get(id=entry_id)
--- ll_project/settings.py
+++ ll_project/settings.py
@@ -123,3 +123,4 @@ STATIC_URL = 'static/'
 # My settings
 LOGIN_REDIRECT_URL = "learning_logs:index"
 LOGOUT_REDIRECT_URL = "learning_logs:index"
+LOGIN_URL = "accounts:login"
