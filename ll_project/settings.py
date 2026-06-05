37c48c9bd39b22b8624e845b72c1d5450e96df8e
zhakiya-r <z.rakhymzhan@yahoo.com>
2026-06-05 16:16:33 +0500

    style(home): update home page with Bootstrap 5

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     # My apps
     "learning_logs",
     "accounts",
+    # Third-party apps
+    "django_bootstrap5",
     # Django default apps
     'django.contrib.admin',
     'django.contrib.auth',
